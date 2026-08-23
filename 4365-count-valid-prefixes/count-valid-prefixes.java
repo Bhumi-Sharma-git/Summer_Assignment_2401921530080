330e876a884da25d63a3c40a83232ebcd14ceb3d
Bhumi-Sharma-git <7073207352bhumi@gmail.com>
2026-08-23 08:54:56 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (99.24%) - LeetSync

---
+++ 4365-count-valid-prefixes/count-valid-prefixes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int zeros = 0;
+        int ones = 0;
+        int ans = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '0') {
+                zeros++;
+            } else {
+                ones++;
+            }
+
+            if (Math.abs(zeros - ones) <= 1) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
