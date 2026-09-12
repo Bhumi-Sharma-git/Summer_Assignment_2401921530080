915e0364a2a45a364d66accc34d843e1e0f7a9ad
Bhumi Sharma <7073207352bhumi@gmail.com>
2026-09-12 16:40:14 +0530

    Time: 2423 ms (5.01%) | Memory: 50.3 MB (11.94%) - LeetSync

---
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProduct(String[] words) {
+        int ans=0;
+        int product=0;
+       for(int i=0;i<words.length;i++){
+        HashSet<Character> set=new HashSet<>();
+        for(char ch:words[i].toCharArray()){
+            set.add(ch);
+        }
+        for(int j=i+1;j<words.length;j++){
+            boolean common=false;
+            for(char ch:words[j].toCharArray()){
+                if(set.contains(ch)){
+                    common=true;
+                    break;
+                }
+            }
+            product=words[i].length()*words[j].length();
+            if(common==false){
+                ans=Math.max(ans,product);
+            }
+        }
+       } 
+       return ans;
+    }
+}
